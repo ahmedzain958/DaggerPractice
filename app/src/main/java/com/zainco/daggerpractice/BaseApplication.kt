ef9d0c64a3a45afe040b7039fdd197e2692e7662
ahmed zain <ahmedzain958@gmail.com>
2019-12-20 09:03:48 +0200

    Initial commit

---
+++ app/src/main/java/com/zainco/daggerpractice/BaseApplication.kt
@@ -0,0 +1,11 @@
+package com.zainco.daggerpractice
+
+import com.zainco.daggerpractice.di.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
+
+class BaseApplication: DaggerApplication() {
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+      return  DaggerAppComponent.builder().application(this).build()
+    }
+}
---
+++ app/src/main/java/com/zainco/daggerpractice/di/AppComponent.kt
@@ -0,0 +1,24 @@
+package com.zainco.daggerpractice.di
+
+import android.app.Application
+import com.zainco.daggerpractice.BaseApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+
+@Component(
+    modules =
+    [AndroidSupportInjectionModule::class,
+        ActivityBuildersModule::class]
+)
+interface AppComponent : AndroidInjector<BaseApplication> {
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        //mandatory step
+        fun build(): AppComponent
+    }
+}
